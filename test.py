f0af1263ddfd25f93e730b25c6a203b308c909a5
Michael Graczyk <michael@mgraczyk.com>
2014-09-03 23:00:27 -0700

    Improved a test

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ import os
 from os import listdir
 from os.path import isfile, join
 
+from itertools import islice
+
 from compiler import GraphCompiler
 from compiler import GraphCompilerError
 from tokenizer import TokenizationError
@@ -15,26 +17,38 @@ def get_tests():
         if os.path.isfile(testPath) and testName.endswith(".graph"):
             yield testPath
 
+def print_truncate(items, maxPrint):
+    it = iter(items)
+    for item in islice(it, maxPrint):
+        print(item)
+
+    remaining = sum(1 for _ in it)
+    if remaining:
+        print("... truncated {} items ...".format(remaining))
+
 ################################################################################
 # Compiler
 ################################################################################
 
 def compiler_test():
+    maxPrint = 40
+
     comp = GraphCompiler()
 
     for testPath in get_tests():
         teststr = open(testPath, "r").read()
-        print(teststr)
+
+        print_truncate(teststr.split("\n"), maxPrint)
         print("-->\n")
 
         try:
             (verts, adj) = comp.compile(teststr)
             print("Vertices = ")
-            [print(v) for v in verts]
+            print_truncate(verts, maxPrint)
             print()
             print("Adj = ")
-            [print(r) for r in adj]
-            print('\n')
+            print_truncate(adj, maxPrint)
+            print()
         except (TokenizationError, GraphCompilerError) as e:
             print(e)
         print()
@@ -45,15 +59,18 @@ def compiler_test():
 ################################################################################
 
 def graph_test():
+    maxPrint = 40
     gTok = GraphCompiler.tokenizer
 
     for testPath in get_tests():
         teststr = open(testPath, "r").read()
-        print(teststr)
+
+        print_truncate(teststr.split("\n"), maxPrint)
         print("-->\n")
 
         try:
-            [print(t) for t in gTok.tokenize(teststr)]
+            tokens = gTok.tokenize(teststr)
+            print_truncate(tokens, maxPrint)
         except TokenizationError as e:
             print(e)
         print()
